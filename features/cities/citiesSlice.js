114c89486a4a1297a39e3a7f8b545ff3695aef22
Giovanni-Serra-Git <giovanni.serra.web@gmail.com>
2025-02-15 21:30:19 +0100

    Change base_url

--- features/cities/citiesSlice.js
+++ features/cities/citiesSlice.js
@@ -114,6 +114,7 @@ function fetchCities() {
             }
 
             const data = await res.json()
+            console.log("Data")
             console.log(data)
             dispatch({type: "city/loaded", payload: data})            
         } catch (error) {
